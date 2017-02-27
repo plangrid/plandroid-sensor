6e68670f9f62331011f14af07a719ee71ba47db4
Harry <harry@plangrid.com>
2017-02-27 13:13:35 -0800

    Update versions and add comments

--- PlanSensor/app/src/main/java/com/plangrid/plansensor/Graph.java
+++ PlanSensor/app/src/main/java/com/plangrid/plansensor/Graph.java
@@ -7,14 +7,19 @@ import com.robinhood.spark.SparkAdapter;
 import com.robinhood.spark.SparkView;
 
 public class Graph extends SparkView {
-    int[] data = new int[]{};
-    SparkAdapter adapter = new GraphAdapter();
+    private int[] data = new int[]{};
+    private final SparkAdapter adapter = new GraphAdapter();
 
     public Graph(Context context, AttributeSet attrs) {
         super(context, attrs);
         setAdapter(adapter);
     }
 
+    /**
+     * Set the data for the graph to display
+     *
+     * @param data - An int[] containing all the datapoints the graph should display
+     */
     public void setData(int[] data) {
         this.data = data;
         adapter.notifyDataSetChanged();
--- PlanSensor/app/src/main/java/com/plangrid/plansensor/MainActivity.java
+++ PlanSensor/app/src/main/java/com/plangrid/plansensor/MainActivity.java
@@ -9,7 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // The graph view to populate with sensor data
         Graph graph = (Graph) findViewById(R.id.graph);
+        // display data in the graph by passing it an int array of data points
         graph.setData(new int[] {1, 5, 6, 4, 2, 1, 1, 3, 8});
     }
 }
