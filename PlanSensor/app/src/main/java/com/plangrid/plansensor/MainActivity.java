b2ea38a9d562fa5a32fe28bf145ef975cbc874f1
Harry <harry@plangrid.com>
2017-07-17 14:41:15 -0700

    Update versions and a little cleanup

--- PlanSensor/app/src/main/java/com/plangrid/plansensor/MainActivity.java
+++ PlanSensor/app/src/main/java/com/plangrid/plansensor/MainActivity.java
@@ -1,11 +1,11 @@
 package com.plangrid.plansensor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
