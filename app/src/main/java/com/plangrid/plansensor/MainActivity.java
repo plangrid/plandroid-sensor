d3f1a80af5f4e51169add06b59f657d513baa28a
Harry <harry@plangrid.com>
2017-09-14 14:49:54 -0700

    Set up for part 2

--- app/src/main/java/com/plangrid/plansensor/MainActivity.java
+++ app/src/main/java/com/plangrid/plansensor/MainActivity.java
@@ -1,14 +1,28 @@
 package com.plangrid.plansensor;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-import java.util.Arrays;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MAX_VALUES = 10;
+    private Sensor sensor;
+    private CompositeDisposable disposables;
+    private LengthLimitedList<Integer> values;
+
+    @BindView(R.id.graph)
+    Graph graph;
 
     // The graph view to populate with sensor data
     @BindView(R.id.graph)
@@ -18,10 +32,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
 
-        // display data in the graph by passing it a list of data points
-        graph.setData(Arrays.asList(1, 5, 6, 4, 2, 1, 1, 3, 8));
+        sensor = new Sensor();
+        values = new LengthLimitedList<>(MAX_VALUES);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        disposables = new CompositeDisposable();
+        disposables.add(sensor.getSensorObservable()
+                              .observeOn(AndroidSchedulers.mainThread())
+                              .subscribe(dataPoint -> {
+                                  Log.v(getClass().getName(), "New Data Point: " + dataPoint.value);
+                                  values.add(dataPoint.value);
+                                  graph.setData(values);
+                              }));
+    }
+
+    @Override
+    protected void onPause() {
+        disposables.dispose();
+        disposables = null;
+        super.onPause();
+    }
+
+    @OnClick(R.id.record)
+    protected void onClickRecord(View view) {
+        // TODO: 9/14/17 Implement recording
+    }
+
+    @OnClick(R.id.play)
+    protected void onClickPlay(View view) {
+        // TODO: 9/14/17 Implement playback
+    }
+    
 }
