8a6afe097f617396c3349faf1b62b341214ee16a
Brad McManus <brad.mcmanus@plangrid.com>
2020-01-27 17:35:38 -0800

    Update January 2020

--- app/src/main/java/com/plangrid/plansensor/MainActivity.java
+++ app/src/main/java/com/plangrid/plansensor/MainActivity.java
@@ -5,24 +5,17 @@ import android.os.Bundle;
 import java.util.Arrays;
 
 import androidx.appcompat.app.AppCompatActivity;
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    // The graph view to populate with sensor data
-    @BindView(R.id.graph)
-    Graph graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
-
         // Display data in the graph by passing it a list of data points.
-        graph.setData(Arrays.asList(1, 5, 6, 4, 2, 1, 1, 3, 8));
+        final GraphView graphView = findViewById(R.id.graph);
+        graphView.setData(Arrays.asList(1, 5, 6, 4, 2, 1, 1, 3, 8));
 
         // TODO(1): Listen to the sensor and display the last ten values in the graph.
     }
